d81e384b29961dbd04e8f237ed6c1f0dbe7e6a7a
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2022-05-09 23:31:59 -0300

    Projeto criado

---
+++ backend/src/main/java/com/MyMovies/dsMovies/DsMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.MyMovies.dsMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsMoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/MyMovies/dsMovies/DsMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyMovies.dsMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
