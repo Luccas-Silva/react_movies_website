d183a267a63c6ed4026f9b0913eeed0c2e6f5d7a
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2022-05-10 17:42:17 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,21 +1,26 @@
-import {ReactComponent as GithubIcon} from 'assents/img/github.svg';
+import { ReactComponent as GithubIcon } from 'assents/img/github.svg';
+import { ReactComponent as TrendyProIcon } from 'assents/img/TrendyPro.svg';
 import './styles.css';
 
-function Navbar(){
-    return(
+function Navbar() {
+    return (
         <header>
-            <nav className="conteiner">
-                <div className="dsmovie-nav-content">
-                    <h1>TrendyPro</h1>
+            <nav className="containar">
+                <div className="dsmovies-nav-content">
+                    <h1>
+                        <TrendyProIcon />
+                        TrendyPro
+                    </h1>
                     <a href="https://github.com/Luccas-Silva">
-                    <div className="dsmovie-contact-conteiner">
-                        <GithubIcon />
-                        <p className=".dsmovie-contact-link">/Luccas-Silva</p>
-                    </div>
+                        <div className="dsmovies-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/Luccas-Silva</p>
+                        </div>
                     </a>
                 </div>
             </nav>
         </header>
-    )
+    );
 }
+
 export default Navbar;
---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,10 @@
+function Form() {
+
+    return(
+        <h1>Página de Formulário</h1>
+    );
+
+
+}
+
+export default Form;
