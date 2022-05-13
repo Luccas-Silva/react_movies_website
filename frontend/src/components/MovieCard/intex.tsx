49a7a5540b6de1b6832ed558746b31b74d01ba63
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2022-05-13 17:02:38 -0300

    Props

--- frontend/src/components/MovieCard/intex.tsx
+++ frontend/src/components/MovieCard/intex.tsx
@@ -1,16 +1,13 @@
 import MovieScore from 'components/MovieScore/intex';
 import { Link } from 'react-router-dom';
+import { Movie } from 'types/movie';
 import './styles.css';
 
-function MovieCard() {
+type Props = {
+    movie: Movie;
+}
 
-    const movie = {
-        id: 1,
-        image: "https://1.bp.blogspot.com/-z1ao0--og0o/YVfX4oXV-OI/AAAAAAAAZx8/TiOcufm-2GIu4W9C1lowKJCcCdltPOSZwCNcBGAsYHQ/s2048/kimetsu-temporada-2-poster_5jqw.jpg",
-        title: "Demon Slayer: Kimetsu no Yaiba",
-        count: 2,
-        score: 4.7
-    };
+function MovieCard( { movie } : Props ){
 
     return (
         <div>
