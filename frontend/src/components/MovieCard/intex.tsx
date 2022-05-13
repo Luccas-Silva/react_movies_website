e316c8ccb281f82e0312b729b732064004bef077
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2022-05-13 17:52:19 -0300

    useParams

--- frontend/src/components/MovieCard/intex.tsx
+++ frontend/src/components/MovieCard/intex.tsx
@@ -16,7 +16,7 @@ function MovieCard( { movie } : Props ){
                 <h3>{movie.title}</h3>
                 <MovieScore />
 
-                <Link to={`/form/1${movie.id}`}>
+                <Link to={`/form/${movie.id}`}>
                   <div className="btn btn-primary dsmovie-btn">Avaliar</div>  
                 </Link>
             </div>
--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,45 +1,12 @@
-import { Link } from 'react-router-dom';
-import './styles.css';
+import FormCard from 'components/FormCard';
+import { useParams } from 'react-router-dom';
 
 function Form() {
 
-    const movie = {
-        id: 1,
-        image: "https://1.bp.blogspot.com/-z1ao0--og0o/YVfX4oXV-OI/AAAAAAAAZx8/TiOcufm-2GIu4W9C1lowKJCcCdltPOSZwCNcBGAsYHQ/s2048/kimetsu-temporada-2-poster_5jqw.jpg",
-        title: "Demon Slayer: Kimetsu no Yaiba",
-        count: 2,
-        score: 4.7
-    };
+    const params = useParams();
 
     return (
-        <div className="dsmovie-form-container">
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="dsmovie-card-bottom-container">
-                <h3>{movie.title}</h3>
-                <form className="dsmovie-form">
-                    <div className="form-group dsmovie-form-group">
-                        <label htmlFor="email">Informe seu email</label>
-                        <input type="email" className="form-control" id="email" />
-                    </div>
-                    <div className="form-group dsmovie-form-group">
-                        <label htmlFor="score">Informe sua avaliação</label>
-                        <select className="form-control" id="score">
-                            <option>1 Star</option>
-                            <option>2 Star</option>
-                            <option>3 Star</option>
-                            <option>4 Star</option>
-                            <option>5 Star</option>
-                        </select>
-                    </div>
-                    <div className="dsmovie-form-btn-container">
-                        <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
-                    </div>
-                </form >
-                <Link to="/">
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
-                </Link>
-            </div >
-        </div >
+        <FormCard movieId={`${params.movieId}`}/>
     );
 }
 
